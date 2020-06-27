718f7277f005d2f8bd73ac462aa75cb52c143e05
Chris Powell <65933702+chrispowelll@users.noreply.github.com>
2020-06-27 23:23:02 +1000

    Created javascript file
    - Includes read more/less button functions

---
+++ js/script.js
@@ -0,0 +1,74 @@
+function readMoreFunctionBen() {
+  var dots = document.getElementById("dotsBen");
+  var moreText = document.getElementById("readMoreBen");
+  var btnText = document.getElementById("readMoreButtonBen");
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+function readMoreFunctionChris() {
+  var dots = document.getElementById("dotsChris");
+  var moreText = document.getElementById("readMoreChris");
+  var btnText = document.getElementById("readMoreButtonChris");
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+function readMoreFunctionDan() {
+  var dots = document.getElementById("dotsDan");
+  var moreText = document.getElementById("readMoreDan");
+  var btnText = document.getElementById("readMoreButtonDan");
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+function readMoreFunctionJohn() {
+  var dots = document.getElementById("dotsJohn");
+  var moreText = document.getElementById("readMoreJohn");
+  var btnText = document.getElementById("readMoreButtonJohn");
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+function readMoreFunctionRomaisa() {
+  var dots = document.getElementById("dotsRomaisa");
+  var moreText = document.getElementById("readMoreRomaisa");
+  var btnText = document.getElementById("readMoreButtonRomaisa");
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
