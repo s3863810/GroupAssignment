d68aca559bdba7feff444cf09f44cfd090ff7ff9
Chris Powell <65933702+chrispowelll@users.noreply.github.com>
2020-07-09 22:59:49 +1000

    Added transitions

--- js/script.js
+++ js/script.js
@@ -1,3 +1,40 @@
+function enterSite() {
+  window.open("personalinformation.html", "_self");
+}
+
+function fadeInPage() {
+  if (!window.AnimationEvent) { return; }
+  var fader = document.getElementById('fader');
+  fader.classList.add('fade-out');
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  if (!window.AnimationEvent) { return; }
+  var anchors = document.getElementsByTagName('a');  
+  for (var idx=0; idx<anchors.length; idx+=1) {
+    if (anchors[idx].hostname !== window.location.hostname || anchors[idx].pathname === window.location.pathname) { continue; }
+    anchors[idx].addEventListener('click', function(event) {
+      var fader = document.getElementById('fader'),
+      anchor = event.currentTarget;
+      var listener = function() {
+        window.location = anchor.href;
+        fader.removeEventListener('animationend', listener);
+      };
+      fader.addEventListener('animationend', listener);
+      event.preventDefault();
+      fader.classList.add('fade-in');
+    });
+  }
+});
+
+window.addEventListener('pageshow', function (event) {
+  if (!event.persisted) {
+    return;
+  }
+  var fader = document.getElementById('fader');
+  fader.classList.remove('fade-in');
+});
+
 function readMoreFunctionBen() {
   var dots = document.getElementById("dotsBen");
   var moreText = document.getElementById("readMoreBen");
